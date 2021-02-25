52d810a6acb81babbc70b0b5f4181012ddb3b037
CampoDiFiori <dudkopiotr@gmail.com>
2021-02-25 19:36:34 +0100

    Working println

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+
+    println!("Czesc, jestem Djanka!");
+    println!("A ty?");
+
+    loop {}
+}
